a0cbbbabfe80b76cfd2beda9a644ecb576553c76
Rohan Dagale <workspacex28@gmail.com>
2024-06-22 17:30:31 +0530

    version 0.1.0

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,19 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export default async function middleware(request: NextRequest) {
-    const path = request.nextUrl.pathname
+    // const path = request.nextUrl.pathname
 
-    const isPublicPath = path === '/login' || path === '/signup' || path === '/'
+    // const isPublicPath = path === '/login' || path === '/signup' || path === '/'
 
-    const token = request.cookies.get('next-auth.session-token')?.value
+    // const token = request.cookies.get('next-auth.session-token')?.value
 
-    if (isPublicPath && token) {
-        return NextResponse.redirect(new URL('/dashboard', request.nextUrl))
-    }
+    // if (isPublicPath && token) {
+    //     return NextResponse.redirect(new URL('/dashboard', request.nextUrl))
+    // }
 
-    if (!isPublicPath && !token) {
-        return NextResponse.redirect(new URL('/login', request.nextUrl))
-    }
+    // if (!isPublicPath && !token) {
+    //     return NextResponse.redirect(new URL('/login', request.nextUrl))
+    // }
 
     return NextResponse.next()
 }
