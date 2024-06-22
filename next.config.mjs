59dd827a032a28b4e6b85cd7706dbf7f04a78bbb
Rohan Dagale <workspacex28@gmail.com>
2024-06-22 16:27:42 +0530

    version 0.1.0

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,17 @@ const nextConfig = {
   },
 
   reactStrictMode: false,
+  trailingSlash: true,
+
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 
   webpack(config) {
     config.module.rules.push({
