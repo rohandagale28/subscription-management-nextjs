6a111fedcaa633599e0c7572e5b1c8dfd188daa8
Rohan Dagale <workspacex28@gmail.com>
2024-06-22 15:18:14 +0530

    version 0.1.0

--- src/app/component/Input.tsx
+++ src/app/component/Input.tsx
@@ -1,23 +1,33 @@
-"use client"
 import { Input } from '@/components/ui/input';
-import React, { useRef } from 'react';
+import React, { ChangeEvent, useRef } from 'react';
 import { InputHTMLAttributes } from 'react';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-    value: string | number | null
-    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    value: string | number;
+    onChange: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
-export function InputDemo({ type, placeholder, value, onChange, name, autoFocus, ...rest }: InputProps) {
+export function InputDemo({
+    type = 'text',
+    placeholder = '',
+    value,
+    onChange,
+    name,
+    autoFocus = false,
+    ...rest
+}: InputProps) {
     const inputRef = useRef<HTMLInputElement>(null);
 
-    return <Input
-        ref={inputRef}
-        type={type} name={name}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        autoFocus={autoFocus}
-        {...rest}
-    />;
+    return (
+        <Input
+            ref={inputRef}
+            type={type}
+            name={name}
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+            autoFocus={autoFocus}
+            {...rest}
+        />
+    );
 }
